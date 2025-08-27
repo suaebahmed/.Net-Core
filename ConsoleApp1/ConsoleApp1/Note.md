1d0785efefb14d615b6cc8f42ac05790b74b3be1
Suaeb Ahmed <suaebahmed12@gmail.com>
2025-08-27 20:45:01 +0600

    Add console app

---
+++ ConsoleApp1/ConsoleApp1/Note.md
@@ -0,0 +1,50 @@
+﻿# Learning Topic on C#
+https://devskill.com/course/detail/professional-programming-with-csharp
+
+- Property
+- Execution step in inheritance
+
+
+- Interface and abstraction
+  ** An interface is a completely "abstract class", which can only contain abstract methods and properties (with empty bodies):
+  ** multiple inheritance is possible with interfaces.
+
+- Lambda expressions and anonymous functions
+  We use lambda expressions to create an anonymous function and others(delegates or expression tree types)
+- Delegates
+  
+
+### Need to learn
+
+- Mediatr library - with command behavioral pattern 
+- CQRS
+- Unit Test - https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-best-practices
+- DI - Instantiated
+- Middleware
+- What are instantiated or injected (e.g., in controllers, handlers, or services) in dotnet?
+- Collections: Learn to use lists, dictionaries, and arrays to manage data.
+
+
+
+### Understanding Fintech
+https://rocketwave.co/understanding-payment-processors/
+
+my playlist on youtube!
+
+### Best Practice
+
+Naming Conventions in C# / .Net Core | by Andreas 🎧 Kagoshima | Medium
+https://medium.com/@a.kago1988/naming-conventions-in-c-crafting-clean-and-declarative-code-14812e1d4c6b
+
+https://stackoverflow.com/questions/150479/order-of-items-in-classes-fields-properties-constructors-methods
+
+
+### Project stucture
+
+```bash
+	WebApi is our main project with controllers. Function is secondary, but not needed now. 
+	Application.Core holds queries, commands, and services with business logic. 
+	Database contains schemas, tables, and procedures. 
+	Domain.Core or Common has shared entities. 
+	The integration folder includes processor folders with integrated processors.
+```
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class MethodInCSharp
+    {
+        public MethodInCSharp()
+        {
+            Func<int, int> square = delegate (int x) { return x * x; };
+            Console.WriteLine(square(10));
+        }
+    }
+
+    abstract class Motorcycle
+    {
+        public void StartEngine() {/* Method statements here */ }
+
+        protected void AddGas(int gallons) { /* Method statements here */ }
+
+        // Derived classes can override the base class implementation.
+        public virtual int Drive(int miles, int speed) { /* Method statements here */ return 1; }
+
+        // Derived classes must implement this.
+        public abstract double GetTopSpeed();
+    }   
+    class Honda : Motorcycle
+    {
+        public override double GetTopSpeed()
+        {
+            return 200; // Example implementation
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var lambda = new LambdaExpressionAndAnonymousFun();
+            var md = new MethodInCSharp();
+            var honda = new Honda();
+            Console.WriteLine($"Honda top speed: {honda.GetTopSpeed()} mph");
+
+
+        }
+    }
+    public class LambdaExpressionAndAnonymousFun
+    {
+        public LambdaExpressionAndAnonymousFun()
+        {
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(10));
+
+            int[] numbers = { 2, 3, 4, 5 };
+            var squaredNumbers = numbers.Select(x => x * x);
+            Console.WriteLine(string.Join(" ", squaredNumbers)); // 4 9 16 25
+
+
+            Action<string> greet = name =>
+            {
+                string greeting = $"Hello {name}!";
+                Console.WriteLine(greeting);
+            };
+            greet("World");
+
+            // Discard one or more parameters
+            Func<int, int, int> add = (x, _) => x + 10;
+
+
+            var sum = (IEnumerable<int> values) =>
+            {
+                int sum = 0;
+                foreach (var value in values)
+                    sum += value;
+
+                return sum;
+            };
+            var sequence = new[] { 1, 2, 3, 4, 5 };
+            var total = sum(sequence);
+            Console.WriteLine(total); // 15
+
+            //Async lambdas
+
+
+            // Lambda expression and tuples
+            Func<(int, int, int), (int, int, int)> doubleThem = tp => (tp.Item1 * 2, tp.Item2 * 2, tp.Item3 * 2);
+            var numbersTuple = (1, 2, 3);
+            var result = doubleThem(numbersTuple);
+            Console.WriteLine($"The set {numbersTuple} doubled is {result}");
+
+            // Lambdas with the standard query operators
+        }
+    }
+}
